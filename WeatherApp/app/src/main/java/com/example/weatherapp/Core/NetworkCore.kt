9025ef86dddee5b4d4ec0af0ef8270584efa44ac
유채원 <ohwow5@sookmyung.ac.kr>
2020-02-19 18:20:00 +0900

    Update sample_project:

--- WeatherApp/app/src/main/java/com/example/weatherapp/Core/NetworkCore.kt
+++ WeatherApp/app/src/main/java/com/example/weatherapp/Core/NetworkCore.kt
@@ -1,29 +1,29 @@
 package com.example.weatherapp.Core
 
 import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
-import retrofit2.converter.gson.GsonConverterFactory
+//import okhttp3.logging.HttpLoggingInterceptor
+//import retrofit2.Retrofit
+//import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+//import retrofit2.converter.gson.GsonConverterFactory
 
 
 object NetworkCore {
-    val api: Retrofit
-    val BASE_URL = ""
-
-    init {
-        var okHttpClient = OkHttpClient()
-            .newBuilder()
-            .addInterceptor(HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY })
-            .build()
-
-        api = Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-            .addConverterFactory(GsonConverterFactory.create())
-            .client(okHttpClient)
-            .build()
-    }
-    inline fun <reified T> getNetworkCore()  = api.create(T::class.java)
+//    val api: Retrofit
+//    val BASE_URL = ""
+//
+//    init {
+//        var okHttpClient = OkHttpClient()
+//            .newBuilder()
+//            .addInterceptor(HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY })
+//            .build()
+//
+//        api = Retrofit.Builder()
+//            .baseUrl(BASE_URL)
+//            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+//            .addConverterFactory(GsonConverterFactory.create())
+//            .client(okHttpClient)
+//            .build()
+//    }
+//    inline fun <reified T> getNetworkCore()  = api.create(T::class.java)
 }
 
